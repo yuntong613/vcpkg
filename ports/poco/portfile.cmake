644a734d1ce79816f076d7482ff499cd2a4dffe9
yuntong <yuntong@thtf.com.cn>
2022-03-24 18:07:50 +0800

    [poco]windows下屏蔽NET_SSL_WIN

--- ports/poco/portfile.cmake
+++ ports/poco/portfile.cmake
@@ -30,7 +30,7 @@ vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
     FEATURES
         pdf         ENABLE_PDF
         netssl      ENABLE_NETSSL
-        netssl      ENABLE_NETSSL_WIN
+        #netssl      ENABLE_NETSSL_WIN
         netssl      ENABLE_CRYPTO
         sqlite3     ENABLE_DATA_SQLITE
         postgresql  ENABLE_DATA_POSTGRESQL
